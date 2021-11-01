db8b92c31128784a2405e776ecffa06a4d172362
Jacob Reckhard <jacobreckhard@gmail.com>
2021-11-01 16:41:52 -0600

    setup example

---
+++ game/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    rl "github.com/chunqian/go-raylib/raylib"
+
+    "runtime"
+)
+
+func init() {
+    runtime.LockOSThread()
+}
+
+func main() {
+    screenWidth := int32(800)
+    screenHeight := int32(450)
+
+    rl.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window")
+    defer rl.CloseWindow()
+    rl.SetTargetFPS(60)
+
+    for !rl.WindowShouldClose() {
+        rl.BeginDrawing()
+        rl.ClearBackground(rl.RayWhite)
+        rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+        rl.EndDrawing()
+    }
+}
+
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/jacobrec/there-is-no-bug
-
-go 1.17
